d84bba7ea77d179ab06dacf3dd10c076ceb513b1
Denis <55002785+DenisMarut@users.noreply.github.com>
2020-12-13 14:39:30 +0100

    Init commit

---
+++ src/test/java/com/dmarut/schoolregister/SchoolRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmarut.schoolregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
